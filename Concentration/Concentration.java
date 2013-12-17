4f63a56155e2e685064b24b6b4e29fc5c1738bc1
archaephyrryx <caufeminecraft+github@gmail.com>
2013-12-17 14:42:49 -0500

    Foobar edits

--- Concentration/Concentration.java
+++ Concentration/Concentration.java
@@ -2,6 +2,33 @@
 class Concentration
 Facilitates playing game of Concentration,
 aka Memory.
+
+eg,
+ if _list is initialized to {"ape","bat","cat","dog","eel","fog","goo","hat"}  
+
+ ...then a partial sample run might look like:  
+          0	 1	 2	 3
+	  4	 5	 6	 7
+	  8	 9	10	11
+	 12	13	14	15
+
+$ turn over > 1
+$ turn over > 4
+          0	ape	 2	 3
+	  ape    5	 6	 7
+	  8	 9	10	11 12	13	14	15 $ turn over> 2 $ turn over> 11
+          0	ape     bat      3
+	 ape     5	 6	 7
+	  8	 9	10	cat
+	 12	13	14	15     
+
+$ Not a match
+
+          0	ape	 2	 3
+	 ape     5	 6	 7
+	  8	 9	10	11
+	 12	13	14	15
+
 ========================================*/
 
 import java.util.ArrayList;
@@ -25,11 +52,22 @@ public class Concentration {
     // *** INSERT YOUR CONSTRUCTOR AND METHODS HERE ***
     public Concentration() {
 	_board = new ArrayList<Tile>(16);
-	
+	_list = new WordList({"ape","bat","cat","dog","eel","fog","goo","hat"});
+	Wordlist _copy = _list.clone()
 
+	for (int i = 0; i < 8; --i) {
+	    _list.addAll(_list)
+	}
+
+	for (int i = 16; i > 0; --i) {
+	    int index = (int) (Math.random()*i);
+	    _board.add(new Tile(_copy.get(index)));
+	    _copy.remove(index);
+	}
 
     }
 
+ 
 
     //DO NOT MODIFY main()
     public static void main( String[] args ) {
--- Concentration/WordList.java
+++ Concentration/WordList.java
@@ -2,6 +2,14 @@ public class WordList {
 
     private ArrayList myList; // contains Strings made up of letters
 
+    public Wordlist() {
+	String[] list = {"ape","bat","cat","dog","eel","fog","goo","hat"};
+	this(list);
+    }
+    public Wordlist(String[] list) {
+	myList = new ArrayList<String>(Array.asList(list));
+    }
+
     public int numWordsOfLength(int len) { 
 	int num = 0;
 	for (String word : myList) {
