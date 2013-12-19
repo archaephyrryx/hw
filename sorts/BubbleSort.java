9f9c7fa3b49fba9246e02a3113b633769880d10e
archaephyrryx <caufeminecraft+github@gmail.com>
2013-12-19 14:41:08 -0500

    Bubblesort implemented

--- sorts/BubbleSort.java
+++ sorts/BubbleSort.java
@@ -10,9 +10,10 @@ public class BubbleSort {
     //postcond: returns an ArrayList of random integers
     //          from lo to hi, inclusive
     public static ArrayList populate( int size, int lo, int hi ) {
-	ArrayList<Integer> retAL = new ArrayList<Integer>();
-
-	//***INSERT YOUR CODE HERE***
+	ArrayList<Integer> retAL = new ArrayList<Integer>(size);
+	
+	for (int i = 0; i < size; ++i)
+	    retAL.add(new Integer((int) (Math.random() * (hi - lo)) + lo));
 
 	return retAL;
     }
@@ -20,8 +21,15 @@ public class BubbleSort {
 
     //randomly rearrange elements of an ArrayList
     public static void shuffle( ArrayList al ) {
-	//***INSERT YOUR CODE HERE***
-
+	for (int i = 0; i < al.size(); ++i) {
+	    Integer replacement;
+	    for (int j = 0; (i + j) < al.size(); ++j) {
+		if ( ((int) (Math.random() * j)) == 0) {
+		    replacement = al.get(i + j);
+		}
+	    }
+	    al.set(i, replacement);
+	}
     }
 
 
@@ -29,28 +37,37 @@ public class BubbleSort {
     // Rearranges elements of input ArrayList
     // postcondition: data's elements sorted in ascending order
     public static void bubbleSortV( ArrayList<Comparable> data ) {
+	boolean sorted = false;
 
-	//***INSERT YOUR CODE HERE***
-
-    }//end bubbleSortV -- O(?)
+	for (int i = 0; i < data.size() && !(sorted) ; ++i) {
+	    for (int j = i+1; j < data.size(); ++j) {
+		sorted = true;
+		int second = data.get(j);
+		int first = data.get(j-i);
+		if (second < first) {
+		    sorted = false;
+		    first -= (second = (first += second) - second);
+		    data.set(j, new Integer(second));
+		    data.set(j-1, new Integer(first));
+		}
+	    }
+	}
+    } // O(N^2)
 
 
     // ArrayList-returning bubbleSort
     // postcondition: order of data's elements unchanged
     //                Returns sorted copy of data.
     public static ArrayList<Comparable> bubbleSort( ArrayList<Comparable> input ) {
-	//declare and initialize empty ArrayList for copying
-	ArrayList<Comparable> data = new ArrayList<Comparable>();
+	ArrayList<Comparable> data = new ArrayList<Comparable>(input);
 
-	//***INSERT YOUR CODE HERE***
-
+	bubbleSortV(data);
 	return data;
-    }//end bubbleSort -- O(?)
+    }//end bubbleSort -- O(N^2)
 
 
     public static void main(String [] args){
 
-	/*===============for VOID methods=============
 	ArrayList glen = new ArrayList<Integer>();
 	glen.add(7);
 	glen.add(1);
@@ -65,9 +82,7 @@ public class BubbleSort {
 	System.out.println( "ArrayList coco before sorting:\n" + coco );
 	bubbleSortV(coco);
 	System.out.println( "ArrayList coco after sorting:\n" + coco );
-	  ============================================*/
 
-	/*==========for AL-returning methods==========
     	ArrayList glen = new ArrayList<Integer>();
 	glen.add(7);
 	glen.add(1);
@@ -87,7 +102,6 @@ public class BubbleSort {
 			    + cocoSorted );
 	System.out.println( "ArrayList coco after sorting:\n" + coco );
 	System.out.println( coco );
-	  ============================================*/
 
     }//end main
 
