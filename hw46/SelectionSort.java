9fe90b2e3e295807f7f85f96c531ea865fd03253
archaephyrryx <caufeminecraft+github@gmail.com>
2013-12-19 18:20:53 -0500

    Finalized BubbleSort, added SelectionSort skeleton from closet

--- hw46/SelectionSort.java
+++ hw46/SelectionSort.java
@@ -1,73 +1,55 @@
 /*======================================
-  class BubbleSort -- implements bubblesort algorithm
+  class SelectionSort -- implements SelectionSort algorithm
   ======================================*/
 
 import java.util.ArrayList;
 
-public class BubbleSort {
+public class SelectionSort {
 
+    //~~~~~~~~~~~~~~~~~~~ HELPER METHODS ~~~~~~~~~~~~~~~~~~~
     //precond: lo < hi && size > 0
     //postcond: returns an ArrayList of random integers
     //          from lo to hi, inclusive
     public static ArrayList populate( int size, int lo, int hi ) {
-	ArrayList<Integer> retAL = new ArrayList<Integer>(size);
-	
-	for (int i = 0; i < size; ++i)
-	    retAL.add(new Integer((int) (Math.random() * (hi - lo)) + lo));
-
+	ArrayList<Integer> retAL = new ArrayList<Integer>();
+	while( size > 0 ) {
+	    //     offset + rand int on interval [lo,hi]
+	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+	    size--;
+	}
 	return retAL;
     }
 
-
     //randomly rearrange elements of an ArrayList
     public static void shuffle( ArrayList al ) {
-	for (int i = 0; i < al.size(); ++i) {
-	    Integer replacement;
-	    for (int j = 0; (i + j) < al.size(); ++j) {
-		if ( ((int) (Math.random() * j)) == 0) {
-		    replacement = al.get(i + j);
-		}
-	    }
-	    al.set(i, replacement);
-	}
+	int randomIndex;
+        for( int i = al.size()-1; i > 0; i-- ) {
+	    //pick an index at random
+            randomIndex = (int)( (i+1) * Math.random() );
+	    //swap the values at position i and randomIndex
+            al.set( i, al.set( randomIndex, al.get(i) ) );
+        }
     }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-    // VOID version of bubbleSort
+    // VOID version of SelectionSort
     // Rearranges elements of input ArrayList
     // postcondition: data's elements sorted in ascending order
-    public static void bubbleSortV( ArrayList<Comparable> data ) {
-	boolean sorted = false;
-
-	for (int i = 0; i < data.size() && !(sorted) ; ++i) {
-	    for (int j = i+1; j < data.size(); ++j) {
-		sorted = true;
-		int second = data.get(j);
-		int first = data.get(j-i);
-		if (second < first) {
-		    sorted = false;
-		    first -= (second = (first += second) - second);
-		    data.set(j, new Integer(second));
-		    data.set(j-1, new Integer(first));
-		}
-	    }
-	}
-    } // O(N^2)
+    public static void selectionSortV( ArrayList<Comparable> data ) {
+    }//end selectionSortV -- O(?)
 
 
-    // ArrayList-returning bubbleSort
-    // postcondition: order of data's elements unchanged
-    //                Returns sorted copy of data.
-    public static ArrayList<Comparable> bubbleSort( ArrayList<Comparable> input ) {
-	ArrayList<Comparable> data = new ArrayList<Comparable>(input);
-
-	bubbleSortV(data);
-	return data;
-    }//end bubbleSort -- O(N^2)
+    // ArrayList-returning selectionSort
+    // postcondition: order of input ArrayList's elements unchanged
+    //                Returns sorted copy of input ArrayList.
+    public static ArrayList<Comparable> selectionSort( ArrayList<Comparable> input ) {
+    }//end selectionSort, O(?)
 
 
-    public static void main(String [] args){
+    public static void main( String [] args ) {
 
+	/*===============for VOID methods=============
 	ArrayList glen = new ArrayList<Integer>();
 	glen.add(7);
 	glen.add(1);
@@ -75,14 +57,16 @@ public class BubbleSort {
 	glen.add(12);
 	glen.add(3);
 	System.out.println( "ArrayList glen before sorting:\n" + glen );
-	bubbleSortV(glen);
+	selectionSortV(glen);
 	System.out.println( "ArrayList glen after sorting:\n" + glen );
 
 	ArrayList coco = populate( 10, 1, 1000 );
 	System.out.println( "ArrayList coco before sorting:\n" + coco );
-	bubbleSortV(coco);
+	selectionSortV(coco);
 	System.out.println( "ArrayList coco after sorting:\n" + coco );
+	  ============================================*/
 
+	/*==========for AL-returning methods==========
     	ArrayList glen = new ArrayList<Integer>();
 	glen.add(7);
 	glen.add(1);
@@ -90,19 +74,20 @@ public class BubbleSort {
 	glen.add(12);
 	glen.add(3);
 	System.out.println( "ArrayList glen before sorting:\n" + glen );
-	ArrayList glenSorted = bubbleSort( glen );
+	ArrayList glenSorted = selectionSort( glen );
 	System.out.println( "sorted version of ArrayList glen:\n" 
 			    + glenSorted );
 	System.out.println( "ArrayList glen after sorting:\n" + glen );
 
       	ArrayList coco = populate( 10, 1, 1000 );
 	System.out.println( "ArrayList coco before sorting:\n" + coco );
-	ArrayList cocoSorted = bubbleSort( coco );
+	ArrayList cocoSorted = selectionSort( coco );
 	System.out.println( "sorted version of ArrayList coco:\n" 
 			    + cocoSorted );
 	System.out.println( "ArrayList coco after sorting:\n" + coco );
 	System.out.println( coco );
+	  ============================================*/
 
     }//end main
 
-}//end class BubbleSort
+}//end class SelectionSort
