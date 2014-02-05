86b989ea1e170b735ba2d6705e09bfa88cf9a188
Peter Duchovni <caufeminecraft+github@gmail.com>
2014-02-05 13:39:12 -0500

    ZBug Exercise
    Completed ZBug class actor and ZBugRunner, both work

--- ZBug/ZBug.java
+++ ZBug/ZBug.java
@@ -3,12 +3,16 @@ import info.gridworld.actor.Bug;
 public class ZBug extends Bug
 {
     private int steps;
+    private int phase;
     private int sideLength;
 
     public ZBug(int length)
     {
         steps = 0;
+	phase = 0;
         sideLength = length;
+	turn();
+	turn();
     }
 
     public void act()
@@ -20,9 +24,14 @@ public class ZBug extends Bug
         }
         else
         {
-            turn();
-            turn();
-            steps = 0;
+	    if (phase == 0) {
+		for (int i = 0; i < 3; ++i) { turn(); }
+	    } else if (phase == 1) {
+		for (int i = 0; i < 5; ++i) { turn(); }
+	    }
+	    if (phase == 2) { return; }
+	    phase++;
+	    steps = 0;
         }
     }
 }
